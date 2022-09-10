d1d180c6a28cc87712aece89829b7f7c2ce233d4
Marcel Claus <geckse@gmx.de>
2022-09-10 15:27:21 +0200

    boilerplaate

---
+++ nodes/FirebaseCloudMessaging/FirebaseCloudMessaging.node.ts
@@ -0,0 +1,72 @@
+
+import { IExecuteFunctions } from 'n8n-core';
+import {
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	NodeOperationError,
+} from 'n8n-workflow';
+
+export class FirebaseCloudMessaging implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Firebase Cloud Messaging',
+		name: 'firebaseCloudMessaging',
+		group: ['transform'],
+		version: 1,
+		description: 'Send Push Notifications to Android, iOS and Web Apps',
+		defaults: {
+			name: 'Firebase Cloud Messaging',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		properties: [
+            {
+				displayName: 'Title',
+				name: 'text',
+				type: 'string',
+				default: '',
+				placeholder: 'Placeholder value',
+				description: 'The message text to send.',
+			},
+			{
+				displayName: 'Text',
+				name: 'title',
+				type: 'string',
+				default: '',
+				placeholder: 'Placeholder value',
+				description: 'The message text to send.',
+			},
+		],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+
+		let item: INodeExecutionData;
+		let title: string;
+
+		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+			try {
+				title = this.getNodeParameter('title', itemIndex, '') as string;
+				item = items[itemIndex];
+
+
+
+			} catch (error) {
+				if (this.continueOnFail()) {
+					items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
+				} else {
+					if (error.context) {
+						error.context.itemIndex = itemIndex;
+						throw error;
+					}
+					throw new NodeOperationError(this.getNode(), error, {
+						itemIndex,
+					});
+				}
+			}
+		}
+
+		return this.prepareOutputData(items);
+	}
+}
